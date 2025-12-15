10589418dd695030f6234a2e3e5b2e4135baff52
이동욱 <du123kim@naver.com>
2025-12-15 10:53:45 +0900

    7주차(12월15일)

---
+++ donguk/week7/BOJ_7682.java
@@ -0,0 +1,18 @@
+package donguk.week7;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_7682 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer sz = new StringTokenizer(bf.readLine());
+
+        int N = Integer.parseInt(sz.nextToken());
+        int K = Integer.parseInt(sz.nextToken());
+        int P = Integer.parseInt(sz.nextToken());
+        int X = Integer.parseInt(sz.nextToken());
+    }
+}
