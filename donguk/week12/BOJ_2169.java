422a23cf94c5ad792bf2401d63fd3d28a9eb6d64
이동욱 <du123kim@naver.com>
2026-01-18 22:50:09 +0900

    12주차(1월18일)

--- donguk/week12/BOJ_2169.java
+++ donguk/week12/BOJ_2169.java
@@ -1,10 +1,13 @@
 package donguk.week12;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class BOJ_2169 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer sz = new StringTokenizer(bf.readLine());
     }
 }
